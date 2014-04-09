7a52c82b63e96d0d9d0571bdf6137077b6a62da3
Timothy Urness <timothy.urness@drake.edu>
2014-04-09 11:57:49 -0500

    added a method

--- Hello.cpp
+++ Hello.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 using namespace std;
 
+void printsomething() {
+  cout << "woah! a method!!!" <<endl;
+}
+
 int main() {
-  cout << "hello everybody"<<endl; //changed this line
-  cout << "this is awesome!!" <<endl; //added this line
+  cout << "hello everybody"<<endl;
+  printsomething();
 }
 
 
