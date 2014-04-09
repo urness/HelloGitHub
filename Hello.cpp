dfae89bb2f757bf3aacdecd624badc49551ce970
Timothy Urness <timothy.urness@drake.edu>
2014-04-09 11:39:32 -0500

    added !!

--- Hello.cpp
+++ Hello.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
   cout << "hello everybody"<<endl; //changed this line
-  cout << "this is awesome" <<endl; //added this line
+  cout << "this is awesome!!" <<endl; //added this line
 }
 
 
